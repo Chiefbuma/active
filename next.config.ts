cceea3a633b61ec6401e7ed7eae519db2b4ca64b
George Buma <georgebuma.jb@gmail.com>
2026-02-18 11:46:39 +0000

    this is he correct next.config.js
    add this so that it com create stana

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+  images: {
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -12,33 +11,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
+  staticPageGenerationTimeout: 120,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
